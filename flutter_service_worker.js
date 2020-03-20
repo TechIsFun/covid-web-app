b33412df41a205b915ddbf1b4540b252af89d651
Andrea Maglie <andrea.maglie@gmail.com>
2020-03-20 18:15:02 +0100

    repo update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,11 +2,11 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "4ab11970e1c396a55e338ad9ca59aace",
-"/main.dart.js": "3b51e71099390fff88f32b781b8be7c7",
+"/main.dart.js": "97261ecf86ae0f6e20a13aff01055681",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "341b85b86551503485e60cb4545e1ed6",
-"/assets/LICENSE": "44bb09f745f3ac873e17f898b4f8a424",
+"/assets/LICENSE": "d2b55d9475eb98f7f132e6b0739a65a4",
 "/assets/AssetManifest.json": "6fc3684c256969076b54757696462122",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
