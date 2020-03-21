4736be86d116b118fdbaaa10aa74835a149fcc8b
Andrea Maglie <andrea.maglie@gmail.com>
2020-03-21 10:04:16 +0100

    repo update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,10 +1,10 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "4ab11970e1c396a55e338ad9ca59aace",
+  "/index.html": "78b46f57e607ee90566ebd25579a690b",
 "/main.dart.js": "6a4b8e2143706884db437e867c000b7b",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/icons/Icon-192.png": "fde2cddc4424cc122f939b77bbab9610",
+"/icons/Icon-512.png": "c82a566865bbb7a5b21e344e6577b1f8",
 "/manifest.json": "5bcee1151db694d24ed40cf99eb91593",
 "/assets/LICENSE": "06adcf8b80f2dc06b4a3043cf3312c75",
 "/assets/AssetManifest.json": "6fc3684c256969076b54757696462122",
